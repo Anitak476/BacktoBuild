c0d1ad5ac4a6c19fab23c9c8255d32aaea1bd3e3
Anita <anitakosuri8@gmail.com>
2025-10-06 15:04:15 +0530

    Week2 Day6 MatrixMultiplication

---
+++ Week2/Day6/MatrixMultiplication.java
@@ -0,0 +1,38 @@
+// Week2 Day6 - Matrix Multiplication using 2D Arrays
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] b = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] result = new int[3][3];
+
+        // Matrix multiplication logic
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        // Display result
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
